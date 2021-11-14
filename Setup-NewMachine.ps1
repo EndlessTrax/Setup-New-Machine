ef3c91f80ff45a957165b3f95dc79d48fd1c2454
Ricky White <ricky@whitelionmedia.com>
2021-11-13 21:09:50 -0500

    Added check to see if packages are already installed, as winget will try install again even if present.

--- Setup-NewMachine.ps1
+++ Setup-NewMachine.ps1
@@ -6,6 +6,7 @@ $WingetPackages = @(
     'Microsoft.PowerShell',
     'Microsoft.VisualStudioCode',
     'JanDeDobbeleer.OhMyPosh',
+    'Python.Python.3'
     'GoLang.Go',
     'GitHub.cli',
     'Microsoft.dotnet',
@@ -25,9 +26,15 @@ $Count = 0
 
 foreach ($Item in $WingetPackages) {
     try {
-        winget install $Item -s winget
-        Write-Output "Installed: $Item"
-        $Count++
+        $listApp = winget list --exact -q $Item
+        if (![String]::Join("", $listApp).Contains($Item)) {
+            winget install $Item -s winget
+            Write-Output "Installed: $Item"
+            $Count++
+        }
+        else {
+            Write-Output "Already installed: $Item"
+        }
     }
     catch {
         Write-Error "Failed to install: $Item"
